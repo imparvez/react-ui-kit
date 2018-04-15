74e84b8d9f57f7e50f0b6ebe18981ff2a188c1ce
Parvez Shaikh <parvezshaikhj@gmail.com>
2018-04-15 23:20:22 +0530

    Image Component Added

--- client/components/App.js
+++ client/components/App.js
@@ -11,7 +11,7 @@ import Image from './Image';
 import CardFooter from './Cards/CardFooter';
 import CardFooterItem from './Cards/CardFooterItem';
 import Input from './Form/Input';
-import Label from './Label'
+import Label from './Label';
 
 export default class App extends React.Component {
   render() {
@@ -147,6 +147,16 @@ export default class App extends React.Component {
 					text: 'This email is invalid',
 				}}
 			/>
+        </section>
+        <section className="section" style={{ width: '480px' }}>
+			<Image src="http://placehold.it/16x16" size="is16X16" style={{ marginBottom: '5px' }} />
+			<Image src="http://placehold.it/24x24" size="is24X24" style={{ marginBottom: '5px' }} />
+			<Image src="http://placehold.it/32x32" size="is32X32" style={{ marginBottom: '5px' }} />
+			<Image src="http://placehold.it/64x64" size="is64X64" style={{ marginBottom: '5px' }} />
+			<Image src="http://placehold.it/128x128" size="is128X128" style={{ marginBottom: '5px' }} />
+			<Image src="http://placehold.it/256x256" size="is256X256" style={{ marginBottom: '5px' }} />
+			<Image src="http://placehold.it/480x480" ratio="isSquare" style={{ marginBottom: '5px' }} />
+			<Image src="http://placehold.it/480x320" ratio="is3By2" style={{ marginBottom: '5px' }} />
         </section>
       </div>);
   }
